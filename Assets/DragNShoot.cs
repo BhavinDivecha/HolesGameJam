a27e1236477f7a0cb611bab1d779e6a1793a992f
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2020-02-22 23:12:32 +0530

    Initial commit

---
+++ Assets/DragNShoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragNShoot : MonoBehaviour
+{
+    public float power;
+    public Rigidbody2D rig;
+
+
+    Vector3 startPos, endPos;
+    Vector2 force;
+    [SerializeField]
+    Vector2 minPower, maxPower;
+
+    public static DragNShoot Instance;
+    private void Awake()
+    {
+        Instance = null;
+        Instance = this;
+        rig = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Shoot(Vector3 sp,Vector3 ep)
+    {
+        startPos = sp;
+        endPos = ep;
+        force = new Vector2(Mathf.Clamp(startPos.x-endPos.x,minPower.x,maxPower.x),Mathf.Clamp(startPos.y-endPos.y,minPower.y,maxPower.y));
+        rig.AddForce(force*power,ForceMode2D.Impulse);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/DragShootLine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragShootLine : MonoBehaviour
+{
+    [SerializeField]
+    LineRenderer line;
+    public Vector3 startPos, endPos;
+    private void Awake()
+    {
+        line = GetComponent<LineRenderer>();
+        line.SetPosition(0,Vector3.zero);
+        line.SetPosition(1,Vector3.zero);
+        line.numCapVertices = 0;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DragNShoot.Instance.rig.velocity = Vector3.zero;
+            line.numCapVertices = 90;
+            startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 newstartPos = new Vector3(startPos.x,startPos.y,0);
+            line.SetPosition(1, newstartPos);
+            line.SetPosition(0, newstartPos);
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            DragNShoot.Instance.rig.velocity = Vector3.zero;
+            endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 newendPos = new Vector3(endPos.x,endPos.y,0);
+            line.SetPosition(0,newendPos);
+        }else if(Input.GetMouseButtonUp(0))
+        {
+            line.SetPosition(0, Vector3.zero);
+            line.SetPosition(1, Vector3.zero);
+            line.numCapVertices = 0;
+            DragNShoot.Instance.Shoot(startPos,endPos);
+        }
+    }
+}
